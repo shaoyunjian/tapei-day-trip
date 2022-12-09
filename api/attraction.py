e077daa0da6e5607c2f1e695098588f89c495b8d
shaoyunjian <shaoyunjian@gmail.com>
2022-12-09 20:33:09 +0800

    Revise sql queries

--- api/attraction.py
+++ api/attraction.py
@@ -14,39 +14,46 @@ cnxpool = connector.connect()
 @attraction.route("/api/attractions")
 def api_attractions():
 	try:
-		cnx = cnxpool.get_connection()
-		cursor = cnx.cursor()
+		connection = cnxpool.get_connection()
+		cursor = connection.cursor()
 		keyword = request.args.get("keyword")
 		page = request.args.get("page", type=int)
 		page_size = 12
 		if keyword: 
 			sql = """
-				SELECT * 
-				FROM `attraction_info` 
-				WHERE `category` = %s or `name` LIKE %s 
+				SELECT `attraction_info`.*, GROUP_CONCAT(`url`) AS `url`
+				FROM `attraction_info`
+				INNER JOIN `image`
+				ON `attraction_info`.`id`=`image`.`attraction_id`
+				WHERE `category` = %s
+				OR `name` LIKE %s
+				GROUP BY `image`.`attraction_id` 
+				ORDER BY `attraction_info`.`id` 
 				LIMIT %s OFFSET %s;
 				"""
-			values = (keyword, "%"+keyword+"%", page_size, page * page_size)
+			
+			values = (keyword, "%"+keyword+"%", page_size + 1, page * page_size)
 			cursor.execute(sql, values)
 			database = cursor.fetchall()
-			values_next_page = (keyword, "%"+keyword+"%", page_size, (page + 1) * page_size)
-			cursor.execute(sql, values_next_page)
-			database_next_page = cursor.fetchall()
 		else:
 			sql = """
-				SELECT * 
-				FROM `attraction_info` 
+				SELECT `attraction_info`.*, GROUP_CONCAT(`url`) AS `url`
+				FROM `attraction_info`
+				INNER JOIN `image`
+				ON `attraction_info`.`id`=`image`.`attraction_id`
+				GROUP BY `image`.`attraction_id` 
+				ORDER BY `attraction_info`.`id` 
 				LIMIT %s OFFSET %s;
 				"""
-			values = (page_size, page * page_size)
+			values = (page_size + 1, page * page_size)
 			cursor.execute(sql, values)
 			database = cursor.fetchall()
-			values_next_page = (page_size, (page + 1) * page_size)
-			cursor.execute(sql, values_next_page)
-			database_next_page = cursor.fetchall()
-
+		
 		attractions_info = []
-		for data in database:
+		
+		for index, data in enumerate(database):
+			if index == 12:
+				break
 			attraction_info = {
 				"id": data[0],
 				"name": data[1],
@@ -60,20 +67,22 @@ def api_attractions():
 				"images": data[9].split(",")
 			}
 			attractions_info.append(attraction_info)
-
-		if len(database_next_page) == 0:
+		
+		if len(database) <= 12:
 			next_page = None
 		else:
 			next_page = page + 1
 
-		return {"nextPage": next_page ,"data": attractions_info}, 200
+		return {
+			"nextPage": next_page ,
+			"data": attractions_info}, 200
 	except:
 		return {
 			"error": True,
 			"message": "Error"}, 500
 	finally:
 		cursor.close()
-		cnx.close()
+		connection.close()
 
 # -----------------------------------------------------
 
@@ -88,18 +97,21 @@ def api_attraction_id(attractionId):
 		}, 400
 
 	try:
-		cnx = cnxpool.get_connection()
-		cursor = cnx.cursor()
+		connection = cnxpool.get_connection()
+		cursor = connection.cursor()
 		sql = """
-			SELECT * 
-			FROM `attraction_info` 
-			WHERE id = %s;
+			SELECT `attraction_info`.*, GROUP_CONCAT(`url`) AS `url`
+			FROM `attraction_info`
+			INNER JOIN `image`
+			ON `attraction_info`.`id`=`image`.`attraction_id`
+			WHERE attraction_info.id = %s
+			GROUP BY `image`.`attraction_id` 
+			ORDER BY `attraction_info`.`id` 
 			"""
 		values = (attractionId,)
 		cursor.execute(sql, values)
 		database = cursor.fetchone()
 		if database:
-			database = list(database)
 			attraction_info = {
 			"id": database[0],
 			"name": database[1],
@@ -124,5 +136,5 @@ def api_attraction_id(attractionId):
 			"message": "Error"}, 500
 	finally:
 			cursor.close()
-			cnx.close()
+			connection.close()
 
